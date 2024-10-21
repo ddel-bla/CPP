b3a5f18ea871aeb8c9c547aa77a62decf3c65195
ddel-bla <ddel-bla@student.42madrid.com>
2024-10-21 06:34:08 +0200

    00

---
+++ CPP00/ex00/src/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	unsigned int	len;
+	std::string		s;
+
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	for (int i = 1; i < argc; i++)
+	{
+		s = argv[i];
+		len = s.length();
+		for (unsigned int k = 0; k < len; k++)
+			s[k] = std::toupper(s.at(k));
+		std::cout << s;
+		if (i < argc - 1)
+			std::cout << " ";
+	}
+	return (std::cout << std::endl, 0);
+}
