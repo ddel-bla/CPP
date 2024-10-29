f23d849c7329136f791d0a3e01cd922fe600f115
ddel-bla <ddel-bla@student.42madrid.com>
2024-10-29 20:35:01 +0100

    test

--- CPP01/ex00/src/main.cpp
+++ CPP01/ex00/src/main.cpp
@@ -13,28 +13,28 @@ int main(int argc, char **argv) {
 	InputCheck(argc, argv);
 
 	// STACK
-    Zombie stack1("ZombieOnStack1");
-    Zombie stack2("ZombieOnStack2");
-    Zombie stack3("ZombieOnStack3");
-    stack1.announce();
-    stack2.announce();
-    stack3.announce();
-    std::cout << std::endl;
+	Zombie stack1("ZombieOnStack1");
+	Zombie stack2("ZombieOnStack2");
+	Zombie stack3("ZombieOnStack3");
+	stack1.announce();
+	stack2.announce();
+	stack3.announce();
+	std::cout << std::endl;
 
 	// HEAP
-    Zombie* heap1 = newZombie("ZombieOnHeap1");
-    Zombie* heap2 = newZombie("ZombieOnHeap2");
-    Zombie* heap3 = newZombie("ZombieOnHeap3");
-    heap1->announce();
-    heap2->announce();
-    heap3->announce();
-    delete heap3;
-    delete heap2;
-    delete heap1;
-    std::cout << std::endl;
+	Zombie* heap1 = newZombie("ZombieOnHeap1");
+	Zombie* heap2 = newZombie("ZombieOnHeap2");
+	Zombie* heap3 = newZombie("ZombieOnHeap3");
+	heap1->announce();
+	heap2->announce();
+	heap3->announce();
+	delete heap3;
+	delete heap2;
+	delete heap1;
+	std::cout << std::endl;
 
 	// CHUMP STACK
-    randomChump("StackChump");
-    std::cout << std::endl;
-    return (0);
+	randomChump("StackChump");
+	std::cout << std::endl;
+	return (0);
 }
