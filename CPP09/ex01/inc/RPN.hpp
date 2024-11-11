776d7584e473338c3ba4a798eb020ee859aa64cf
ddel-bla <ddel-bla@student.42madrid.com>
2024-11-11 01:37:14 +0100

    xç

--- CPP09/ex01/inc/RPN.hpp
+++ CPP09/ex01/inc/RPN.hpp
@@ -1,37 +1,37 @@
-#ifndef  RPN_HPP
-# define  RPN_HPP
+#ifndef RPN_HPP
+#define RPN_HPP
 
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <cctype>
 #include <deque> 
+#include <limits>
 
 class RPN
 {
-    protected:
-
-    private:
-        std::deque<int> num;
-    
-    public:
-        ~RPN(void);
-        RPN(void);
-        RPN(const RPN &cp);
+	private:
+		std::deque<int> num;
+	public:
+		~RPN(void);
+		RPN(void);
+		RPN(const RPN &cp);
 		RPN& operator=(const RPN &other);
-        void pushfrontQueue(int n);
-        void pushbackQueue(int n);
-        void popfrontQueue();
-        void popbackQueue();
-        void printDeque();
-        int getSize();
-        int operator[](size_t i);
+		void pushfrontQueue(int n);
+		void pushbackQueue(int n);
+		void popfrontQueue();
+		void popbackQueue();
+		void printDeque();
+		int getSize();
+		int operator[](size_t i);
 
-        class ExceptionErr: public std::exception {
-            virtual const char* what() const throw() {
-                return "Err";
-            }
-        };
+		void calculate(RPN& num, char simbol);
+		void aux(RPN& num, int n);
+		class ExceptionErr: public std::exception {
+			virtual const char* what() const throw() {
+				return "Error";
+			}
+		};
 };
 
 #endif
--- CPP09/ex01/src/RPN.cpp
+++ CPP09/ex01/src/RPN.cpp
@@ -1,62 +1,97 @@
-#include "RPN.hpp"
+#include "../inc/RPN.hpp"
 
-RPN::~RPN()
-{
+RPN::~RPN() {
     //std::cout << "RPN destructor called" << std::endl;
 }
 
-RPN::RPN( void )
-{
+RPN::RPN( void ) {
     //std::cout << "RPN constructor called" << std::endl;
 }
 
-RPN& RPN::operator=(const RPN &other)
-{
+RPN& RPN::operator=(const RPN &other) {
 	//std::cout << "RPN copy assignment operator called" << std::endl;
     this->num = other.num;
 	return *this;
 }
 
-RPN::RPN(const RPN &cp)
-{
+RPN::RPN(const RPN &cp) {
 	//std::cout << "RPN copy constructor called" << std::endl;
 	*this = cp;
 }
 
-void RPN::pushfrontQueue(int n)
-{
+void RPN::pushfrontQueue(int n) {
 	this->num.push_front(n);
 }
 
-void RPN::pushbackQueue(int n)
-{
+void RPN::pushbackQueue(int n) {
 	this->num.push_back(n);
 }
 
-void RPN::popfrontQueue()
-{
+void RPN::popfrontQueue() {
 	this->num.pop_front();
 }
 
-void RPN::popbackQueue()
-{
+void RPN::popbackQueue() {
 	this->num.pop_back();
 }
 
-void RPN::printDeque()
-{
+void RPN::printDeque() {
 	for (size_t i = 0; i < this->num.size(); ++i)
         std::cout << this->num[i] << std::endl;
 }
 
-int RPN::operator[](size_t i) 
-{
+int RPN::operator[](size_t i) {
     if(this->num.size() <= i)
 		throw RPN::ExceptionErr();
 	return this->num.at(i);
 }
 
-int RPN::getSize()
-{
+int RPN::getSize() {
 	return (num.size());
+}
+
+void RPN::aux(RPN& num, int result) {
+	num.popfrontQueue();
+	num.popfrontQueue();
+	num.pushfrontQueue(result);
+}
+
+void RPN::calculate(RPN& num, char symbol) {
+	if (num.getSize() < 2)
+		throw RPN::ExceptionErr();
+
+	int right = num[0];
+	int left = num[1];
+
+	switch (symbol) {
+		case '+':
+			if ((left > 0 && right > std::numeric_limits<int>::max() - left) ||
+			    (left < 0 && right < std::numeric_limits<int>::min() - left))
+				throw RPN::ExceptionErr();
+			aux(num, left + right);
+			break;
+
+		case '-':
+			if ((left < 0 && right > std::numeric_limits<int>::max() + left) ||
+			    (left > 0 && right < std::numeric_limits<int>::min() + left))
+				throw RPN::ExceptionErr();
+			aux(num, left - right);
+			break;
+		case '*':
+			if (left != 0 && (right > std::numeric_limits<int>::max() / left || right < std::numeric_limits<int>::min() / left))
+				throw RPN::ExceptionErr();
+			aux(num, left * right);
+			break;
+
+		case '/':
+			if (right == 0)
+				throw RPN::ExceptionErr();
+			if (left == std::numeric_limits<int>::min() && right == -1)
+				throw RPN::ExceptionErr();
+			aux(num, left / right);
+			break;
+
+		default:
+			throw RPN::ExceptionErr();
+	}
 }
